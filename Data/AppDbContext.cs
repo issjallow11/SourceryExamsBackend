d068e1dcd1f08600a1ad1ed999b001674426c1d3
Ismaila Jallow <ismailaj@cayorenterprises.com>
2021-08-14 13:45:07 +0000

    seeding sample data for testing

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -13,6 +13,22 @@ namespace DatabaseGeneration.Data
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Product>(entity => { entity.HasIndex(p => p.Name).IsUnique(); });
+            
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 1, Name = "Burger",Currency = "50", Category = "launch"
+            },
+            new Product
+            {
+                Id = 2, Name = "bread",Currency = "25", Category = "breakfast"
+ 
+            },
+            new Product
+            {
+                Id = 3, Name = "fish",Currency = "35", Category = "dinner"
+ 
+            }
+            );
         }
         
         public DbSet<Product> Products { get; set; }
--- Migrations/20210814134018_initital.Designer.cs
+++ Migrations/20210814134018_initital.Designer.cs
@@ -2,14 +2,16 @@
 using DatabaseGeneration.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatabaseGeneration.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210814134018_initital")]
+    partial class initital
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
